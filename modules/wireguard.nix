4c3a67b7ab9a5050b4b3759fb18cc4715f7c7f14
Nikolay Amiantov <ab@fmap.me>
2025-10-01 15:43:35 +0000

    Add AmneziaWG

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -9,14 +9,24 @@ with lib; let
   cfg = rootCfg.wireguard;
 
   dev = "vpn-wg";
+  amneziaDev = "vpn-amneziawg";
   port = 51820;
+  # Hide as a GoldSrc game.
+  amneziaPort = 27015;
 
   peerModule = {...}: {
     options = {
       ip = mkOption {
-        type = types.str;
+        type = types.nullOr types.str;
         description = ''
           Peer's private IP address.
+        '';
+      };
+
+      amneziaIp = mkOption {
+        type = types.nullOr types.str;
+        description = ''
+          Peer's AmneziaWG private IP address.
         '';
       };
 
@@ -29,19 +39,30 @@ with lib; let
     };
   };
 
-  mkPeer = peer: {
-    allowedIPs = ["${peer.ip}/32"];
-    inherit (peer) publicKey;
-  };
+  mkPeer = getIp: peer: let
+    ip = getIp peer;
+  in
+    optional (ip != null) {
+      allowedIPs = ["${ip}/32"];
+      inherit (peer) publicKey;
+    };
 in {
   options = {
     multivpn.wireguard = {
-      enable = mkEnableOption "OpenVPN support";
+      enable = mkEnableOption "Wireguard support";
 
       ip = mkOption {
         type = types.str;
         default = "10.0.174.1";
-        description = "Network subnet that OpenVPN uses.";
+        description = "Network subnet that Wireguard uses.";
+      };
+
+      enableAmnezia = mkEnableOption "AmneziaWG support";
+
+      amneziaIp = mkOption {
+        type = types.str;
+        default = "10.0.177.1";
+        description = "Network subnet that AmneziaWG uses.";
       };
 
       privateKeyFile = mkOption {
@@ -52,52 +73,70 @@ in {
       peers = mkOption {
         type = types.listOf (types.submodule peerModule);
         default = {};
-        description = "WireGuard peers.";
+        description = "WireGuard/AmneziaWG peers.";
       };
     };
   };
 
-  config = mkIf (rootCfg.enable && cfg.enable) {
-    multivpn.vpnInterfaces = [dev];
+  config = mkIf rootCfg.enable (mkMerge [
+    (mkIf cfg.enable {
+      multivpn.vpnInterfaces = [dev];
 
-    networking = {
-      firewall.allowedUDPPorts = [port];
-      # FIXME: IPv6
-      wireguard.interfaces.${dev} = {
-        ips = ["${cfg.ip}/24"];
-        privateKeyFile = cfg.privateKeyFile;
-        listenPort = port;
-        peers = map mkPeer cfg.peers;
+      networking = {
+        firewall.allowedUDPPorts = [port];
+        # FIXME: IPv6
+        wireguard.interfaces.${dev} = {
+          ips = ["${cfg.ip}/24"];
+          privateKeyFile = cfg.privateKeyFile;
+          listenPort = port;
+          peers = concatMap (mkPeer (peer: peer.ip)) cfg.peers;
+        };
       };
-    };
 
-    systemd.services.vpn-credentials-wireguard = {
-      description = "Prepare the client credentials for Wireguard.";
-      wantedBy = ["multi-user.target"];
-      path = with pkgs; [wireguard-tools];
-      serviceConfig = {
-        Type = "oneshot";
-        StateDirectory = "vpn-credentials";
-        StateDirectoryMode = "0700";
-        WorkingDirectory = "/var/lib/vpn-credentials";
+      systemd.services.vpn-credentials-wireguard = {
+        description = "Prepare the client credentials for Wireguard.";
+        wantedBy = ["multi-user.target"];
+        path = with pkgs; [wireguard-tools];
+        serviceConfig = {
+          Type = "oneshot";
+          StateDirectory = "vpn-credentials";
+          StateDirectoryMode = "0700";
+          WorkingDirectory = "/var/lib/vpn-credentials";
+        };
+        script = ''
+          mkdir -p wireguard
+          domain=${escapeShellArg rootCfg.domain}
+          port=${toString port}
+          public=$(wg pubkey < ${escapeShellArg cfg.privateKeyFile})
+          cat > wireguard/wg.conf <<EOF
+          [Interface]
+          PrivateKey = <private key>
+          Address = <ip>/32
+
+          [Peer]
+          Endpoint = $domain:$port
+          PublicKey = $public
+          AllowedIPs = 0.0.0.0/0
+          PersistentKeepalive = 25
+          EOF
+        '';
       };
-      script = ''
-        mkdir -p wireguard
-        domain=${escapeShellArg rootCfg.domain}
-        port=${toString port}
-        public=$(wg pubkey < ${escapeShellArg cfg.privateKeyFile})
-        cat > wireguard/wg.conf <<EOF
-        [Interface]
-        PrivateKey = <private key>
-        Address = <ip>/32
+    })
 
-        [Peer]
-        Endpoint = $domain:$port
-        PublicKey = $public
-        AllowedIPs = 0.0.0.0/0
-        PersistentKeepalive = 25
-        EOF
-      '';
-    };
-  };
+    (mkIf cfg.enableAmnezia {
+      multivpn.vpnInterfaces = [amneziaDev];
+
+      networking = {
+        firewall.allowedUDPPorts = [amneziaPort];
+        # FIXME: IPv6
+        wireguard.interfaces.${amneziaDev} = {
+          type = "amneziawg";
+          ips = ["${cfg.amneziaIp}/24"];
+          privateKeyFile = cfg.privateKeyFile;
+          listenPort = amneziaPort;
+          peers = concatMap (mkPeer (peer: peer.amneziaIp)) cfg.peers;
+        };
+      };
+    })
+  ]);
 }
