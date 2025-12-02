bc39dac03354e1497372d20d96b3c52f960931ae
Nikolay Amiantov <ab@fmap.me>
2025-12-02 23:34:20 +0700

    Move to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,21 +1,29 @@
 {
   inputs = {
-    flake-compat.url = "github:edolstra/flake-compat";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {...}: {
-    nixosModules.default.imports = [
-      ./modules/common.nix
-      ./modules/iodine.nix
-      ./modules/mtprotoproxy.nix
-      ./modules/openvpn.nix
-      ./modules/wireguard.nix
-      ./modules/xray.nix
-      ./modules/vless.nix
-      ./modules/vless-reality.nix
-      ./modules/ss2022.nix
-      ./modules/ss-legacy.nix
-      ./modules/socks5.nix
-    ];
-  };
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
+      flake.nixosModules.default.imports = [
+        ./modules/common.nix
+        ./modules/iodine.nix
+        ./modules/mtprotoproxy.nix
+        ./modules/openvpn.nix
+        ./modules/wireguard.nix
+        ./modules/xray.nix
+        ./modules/vless.nix
+        ./modules/vless-reality.nix
+        ./modules/ss2022.nix
+        ./modules/ss-legacy.nix
+        ./modules/socks5.nix
+      ];
+
+      systems = ["x86_64-linux"];
+
+      perSystem = {pkgs, ...}: {
+        formatter = pkgs.alejandra;
+      };
+    });
 }
