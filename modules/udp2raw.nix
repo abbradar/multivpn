efd0cc543a32f609d1189741819d743c9c2439ce
Nikolay Amiantov <ab@fmap.me>
2026-01-24 14:28:42 +0700

    WIP

--- modules/udp2raw.nix
+++ modules/udp2raw.nix
@@ -1,31 +1,105 @@
-{ config, ... }:
-
 {
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.multivpn.udp2raw;
+
+  instanceModule = types.submodule {
+    options = {
+      address = mkOption {
+        type = types.str;
+        description = "Address to listen on.";
+      };
+
+      port = mkOption {
+        type = types.int;
+        description = "Port to listen on.";
+      };
+
+      destination = mkOption {
+        type = types.str;
+        description = "Destination address to forward to.";
+      };
+    };
+  };
+in {
+  options = {
+    multivpn.udp2raw = {
+      servers = mkOption {
+        type = types.attrsOf instanceModule;
+        default = {};
+        description = "UDP2RAW servers.";
+      };
+
+      clients = mkOption {
+        type = types.attrsOf instanceModule;
+        default = {};
+        description = "UDP2RAW clients.";
+      };
+    };
+  };
+
   config = {
-    systemd.services = mapAttrs' (name: iface:
-      nameValuePair "multivpn-udp2raw-server-${name}" {
-        description = "UDP2RAW server for ${name}.";
-        wantedBy = ["multi-user.target"];
-        wants = ["network.target"];
-        after = ["network.target"];
-        unitConfig = {
-          StartLimitIntervalSec = 0;
-        };
-        serviceConfig = {
-          Restart = "always";
-          RestartSec = 5;
-          # We effectively disable mtu-warn.
-        ExecStart = [
-	  "${pkgs.udp2raw}/bin/udp2raw"
-	  "-s"
-	  "-l" "${iface.address}:${toString iface.port}"
-	  "-r" iface.destination
-          "-a"
-	  "--mtu-warn" "1500"
-	  "--cipher-mode" "none"
-	  "--auth-mode" "none"
-        ];
-        };
-      })
+    systemd.services = mkMerge [
+      (mapAttrs' (name: iface:
+        nameValuePair "multivpn-udp2raw-server-${name}" {
+          description = "UDP2RAW server for ${name}.";
+          wantedBy = ["multi-user.target"];
+          wants = ["network.target"];
+          after = ["network.target"];
+          serviceConfig = {
+            Restart = "always";
+            RestartSec = 5;
+            # We effectively disable mtu-warn.
+            ExecStart = [
+              "${pkgs.udp2raw}/bin/udp2raw"
+              "-s"
+              "-l"
+              "${iface.address}:${toString iface.port}"
+              "-r"
+              iface.destination
+              "-a"
+              "--mtu-warn"
+              "1500"
+              "--cipher-mode"
+              "none"
+              "--auth-mode"
+              "none"
+            ];
+          };
+        })
+      cfg.servers)
+
+      (mapAttrs' (name: iface:
+        nameValuePair "multivpn-udp2raw-client-${name}" {
+          description = "UDP2RAW client for ${name}.";
+          wantedBy = ["multi-user.target"];
+          wants = ["network.target"];
+          after = ["network.target"];
+          serviceConfig = {
+            Restart = "always";
+            RestartSec = 5;
+            ExecStart = [
+              "${pkgs.udp2raw}/bin/udp2raw"
+              "-c"
+              "-l"
+              "${iface.address}:${toString iface.port}"
+              "-r"
+              iface.destination
+              "-a"
+              "--mtu-warn"
+              "1500"
+              "--cipher-mode"
+              "none"
+              "--auth-mode"
+              "none"
+            ];
+          };
+        })
+      cfg.clients)
+    ];
   };
 }
